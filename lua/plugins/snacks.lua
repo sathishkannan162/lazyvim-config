d96986d35b8b4466da2ad46fde686f63a2143902
Sathish Kannan <sathish.m@JY472NGPHF.local>
2025-12-11 18:38:03 +0530

    Update snacks config

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      silent = "true",
+      indent = {
+        hl = "SnacksIndent",
+        animate = { enabled = false },
+      },
+    },
+  },
+}
